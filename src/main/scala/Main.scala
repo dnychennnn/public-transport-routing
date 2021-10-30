c9f929e178524b4364485f50fe0c57b164784506
ychen <yung-yu.chen@autoscout24.com>
2021-10-30 12:59:46 +0200

    fix git cache

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,25 @@
+import dijkstra.DirectedGraphOps.DirectedGraphOps
 import InputReader.readInput
+import OutputBuilder.buildOutput
+import dijkstra.Helpers.mapAlphabetToIndex
+import dijkstra.{EdgeWeightedDigraph, ShortestPath, ShortestPathCalc}
+
+import scala.language.postfixOps
 
 object Main extends App {
 
   val (edges, routeQuery, nearByQuery) = readInput()
 
+  val graph =
+    EdgeWeightedDigraph().addEdges(edges)
+  println(graph)
+  val sp: Either[String, ShortestPathCalc] =
+    ShortestPath.run(graph, routeQuery.source)
+  val destinationAsIndex: Int = mapAlphabetToIndex(routeQuery.destination)
+
+  val resultPath = sp.flatMap(_.pathTo(destinationAsIndex))
+  val resultDis = sp.flatMap(_.distToV(destinationAsIndex))
+  val output = buildOutput(resultPath, resultDis)
+
+  println(output.right.get)
 }
